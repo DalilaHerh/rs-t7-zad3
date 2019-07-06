1cd333c2c97296d71c7dc472c47424ae72149e4c
Dalila <dalila.herh@gmail.com>
2019-07-06 12:05:56 +0200

    ispravljen

--- src/ba/unsa/etf/rs/Main.java
+++ src/ba/unsa/etf/rs/Main.java
@@ -1,20 +1,32 @@
 package ba.unsa.etf.rs;
-
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Comparator;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner unos = new Scanner(System.in);
-        System.out.println("Unesite listu brojeva: \t (0 za kraj)");
-        ArrayList<Integer> listaBrojeva = new ArrayList<>();
-        int pomocni = unos.nextInt();
-        while(pomocni!=0) {
-            listaBrojeva.add(pomocni);
-            pomocni = unos.nextInt();
+    private static Integer SumaCifara(Integer n){
+        Integer suma = 0;
+        while(n != 0){
+            suma = suma + (n%10);
+            n = n / 10;
         }
-        listaBrojeva.stream().sorted().forEach(System.out::println);
+        return  suma;
+    }
 
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Koliko želite unijeti brojeva?");
+        int brojevi = s.nextInt();
+        int[] arr = new int[brojevi];
+        System.out.println("Unesite brojeve: ");
+        for(int i = 0; i < brojevi; i++){
+            arr[i] = s.nextInt();
+        }
+
+        arr = Arrays.stream(arr).boxed().sorted((x, y) -> SumaCifara(x) - SumaCifara(y)).mapToInt(i -> i).toArray();
+        for(int e : arr){
+            System.out.print(e + " ");
+        }
     }
 }
